f357ca9552af1f2e927f0eb9e4191d41e3cbb0f1
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-16 12:31:10 -0500

    Finished Game

---
+++ Main/script.js
@@ -0,0 +1,51 @@
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var options = ["R", "P", "S"];
+
+var game = function() {
+    var userChoice = window.prompt("Enter: R, P, or S:");
+
+    if(!userChoice) {
+        alert("Please try again!")
+        return;
+    }
+
+    userChoice = userChoice.toUpperCase();
+
+    var index = Math.floor(Math.random() * options.length);
+    var computerChoice = options[index];
+
+    window.alert("The computer chose " + computerChoice);
+
+    if (userChoice === computerChoice) {
+        ties++;
+        window.alert("It's a tie!");
+    
+      } else if (
+        (userChoice === "R" && computerChoice === "S") || 
+        (userChoice === "P" && computerChoice === "R") || 
+        (userChoice === "S" && computerChoice === "P")
+      ) {
+        wins++;
+        window.alert("You win!");
+    
+      } else {
+        losses++;
+        window.alert("You lost!");
+      }
+    
+      window.alert(
+        "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+      );
+    
+      var playAgain = window.confirm("Play again?");
+    
+      if (playAgain) {
+        game();
+      }
+    };
+    
+    game();
+    
